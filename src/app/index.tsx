7c413db680eee730a2006685f51065dc4b59b9b8
Ruslan Ivanov <ruslan4432013@gmail.com>
2023-03-12 17:53:03 +0300

    remove use state in app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactElement, useEffect } from "react";
+import { FC, ReactElement } from "react";
 
 import "@shared/styles/globals.scss";
 import { ErrorBoundary } from "@shared/ui/error-boundary";
@@ -7,15 +7,6 @@ import { Offline } from "@src/shared/ui/offline";
 import cn from "classnames";
 
 const App: FC = (): ReactElement => {
-  useEffect(() => {
-    if (
-      !window.__PRELOADED_STATE__?.theme?.theme &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      /* Empty */
-    }
-  }, []);
-
   return (
     <ErrorBoundary>
       <div className={cn("app-wrapper")}>
